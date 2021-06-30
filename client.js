7d6de3830e0e91d600996962ee1ad8630271747d
Miguel Jimenez <migang9@gmail.com>
2021-06-30 13:50:51 -0400

    third step: Snake Initials

--- client.js
+++ client.js
@@ -1,20 +1,23 @@
 const net = require("net");
+//const { stdin } = require("process");
+
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
     host: '135.23.223.133',
-    port: 50542
+    port: 50542, }, () => {
+    console.log("Connection established!");
+    conn.on('data', (data) => {
+      console.log('message from server', data);
+    });
   });
 
   // interpret incoming data as text
+
   conn.setEncoding("utf8");
 
-  const name = 'Miguel';
-  conn.write(`${name} is starting to play!`);
-
-  conn.on('data', (data) => {
-    console.log('message from server', data);
-  });
+  const name = 'MAJ';
+  conn.write(`Name: ${name}`);
 
   return conn;
 };
