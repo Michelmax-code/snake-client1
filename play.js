d69d197c336528022d8482f4f6e5c3d4abac752b
Miguel Jimenez <migang9@gmail.com>
2021-06-30 13:23:46 -0400

    second step: Connection Module

--- play.js
+++ play.js
@@ -1,24 +1,6 @@
-const net = require("net");
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: 50542
-  });
+const connect = require('./client');
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  const name = 'Miguel';
-  conn.write(`${name} is starting to play!`);
-
-  conn.on('data', (data) => {
-    console.log('message from server', data);
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
