8d6568ac8f11cdf7e3d4e7ea3a24820d90790a6e
Miguel Jimenez <migang9@gmail.com>
2021-06-30 20:19:01 -0400

    Constants

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
 const net = require("net");
+const {IP, PORT} = require('./constants');
 //const { stdin } = require("process");
 
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: 50542, }, () => {
+    host: IP,
+    port: PORT, }, () => {
     console.log("Connected with game server!");
     conn.write(`Name: MAJ`);
     conn.on('data', (data) => {
